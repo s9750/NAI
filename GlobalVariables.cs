99896f68ea86a6abb2603bad95b589c10bc53172
s9750 <s9750@pjwstk.edu.pl>
2014-06-14 20:58:06 +0200

    dynamicTownGeneration

--- GlobalVariables.cs
+++ GlobalVariables.cs
@@ -10,6 +10,6 @@ namespace ZadanieKomiwojazera
     {
         public static Random random = new Random();
 
-        public static int[] miasta = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 };
+        public static int[] miasta;
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ZadanieKomiwojazera
@@ -10,9 +11,10 @@ namespace ZadanieKomiwojazera
     {
         static void Main(string[] args)
         {
+            wygenerujMiasta(ilosc: 10);
             ReprezentacjaPorzadkowa repr = new ReprezentacjaPorzadkowa();
             Console.WriteLine("{1,12} {0}", displayHelper(GlobalVariables.miasta),"Trasa:");
-            for (int i = 1; i < 101; i++)
+            for (int i = 1; i < 100001; i++)
             {
                 int[] rodzic = repr.generatoRodzicow(GlobalVariables.miasta, new int[GlobalVariables.miasta.Length], 0);
                 string rodzicString = String.Format("Rodzic[{0}]:", i);
@@ -20,9 +22,19 @@ namespace ZadanieKomiwojazera
 
                 Console.WriteLine("{1,12} {0}", displayHelper(rodzic),rodzicString);
                 Console.WriteLine("{1,12} {0}", displayHelper(repr.odczytTrasy(rodzic)),trasaString);
+                Thread.Sleep(100);
             }
 
             Console.ReadKey();
+        }
+
+        private static void wygenerujMiasta(int ilosc)
+        {
+            GlobalVariables.miasta = new int[ilosc];
+            for (int i = 0; i < ilosc; i++)
+            {
+                GlobalVariables.miasta[i] = i + 1;
+            }
         }
 
         private static string displayHelper(int[] data)
